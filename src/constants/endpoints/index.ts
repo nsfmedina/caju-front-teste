6d7c90ee8e577123ace3655aef45ea2d5e39d037
Lucas Medina <lucass.medina@gmail.com>
2024-09-23 18:47:13 -0300

    fix: changes file extension to .ts

---
+++ src/constants/endpoints/index.ts
@@ -0,0 +1,6 @@
+export const ENDPOINTS = {
+    GET_REGISTRATIONS: `http://localhost:3000/registrations`,
+    POST_REGISTRATIONS: `http://localhost:3000/registrations`,
+    PUT_REGISTRATIONS: `http://localhost:3000/registrations`,
+    DELETE_REGISTRATIONS: `http://localhost:3000/registrations`,
+}
--- src/constants/endpoints/index.tsx
+++
@@ -1,8 +0,0 @@
-const { VITE_API_BASE_URL } = import.meta.env;
-
-export const ENDPOINTS = {
-    GET_REGISTRATIONS: `${VITE_API_BASE_URL}/registrations`,
-    POST_REGISTRATIONS: `${VITE_API_BASE_URL}/registrations`,
-    PUT_REGISTRATIONS: `${VITE_API_BASE_URL}/registrations`,
-    DELETE_REGISTRATIONS: `${VITE_API_BASE_URL}/registrations`,
-}
